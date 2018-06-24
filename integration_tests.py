f3db38c65dc029bcd2edfac41f5423f6326b7b3b
madamczak <fakeemail@tro.lolo>
2018-06-24 22:53:58 +0200

    Refactor normalize method usage

--- integration_tests.py
+++ integration_tests.py
@@ -1,6 +1,4 @@
-import unittest, os
-
-import inspect
+import unittest, os, datetime
 from OperationUtils.db_operations import DataBase
 from Collectors.BrandsCollector import BrandsCollector
 from Collectors.LinksCollector import LinksCollector
@@ -10,6 +8,10 @@ from cars import CarDataCollector
 class SeparateCollectorsTest(unittest.TestCase):
     def setUp(self):
         self.separateDBname = "separate_collectors_test.db"
+
+        if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), self.separateDBname)):
+            os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), self.separateDBname))
+
         self.database = DataBase(self.separateDBname)
         CarDataCollector.constructDBTables(self.database)
 
@@ -18,34 +20,41 @@ class SeparateCollectorsTest(unittest.TestCase):
         os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), self.separateDBname))
 
     def testSeparateCollectors(self):
-        brandsCollector = BrandsCollector(self.separateDBname)
-        numberOfBrands = brandsCollector.Collect(limit=20)
+        brandsCollector = BrandsCollector(self.database)
+        numberOfBrands, brandsStartTime = brandsCollector.Collect(limit=20)
         self.assertTrue(brandsCollector.db.valueIsPresentInColumnOfATable("Rover", "brandname", "cars_brand"))
         self.assertTrue(brandsCollector.db.valueIsPresentInColumnOfATable("Honda", "brandname", "cars_brand"))
         self.assertTrue(brandsCollector.db.valueIsPresentInColumnOfATable("Mercury", "brandname", "cars_brand"))
+        self.assertLess((datetime.datetime.now() - brandsStartTime).total_seconds(), 240)
 
         #todo: inconsistency between brands limit and number of brands collected (models and version counted as brands)
         self.assertGreater(numberOfBrands, 20)
         self.assertGreater(brandsCollector.db.countRecordsInTable("cars_brand"), 20)
         #todo: by 1 error below
         self.assertEqual(brandsCollector.db.countRecordsInTable("cars_brand"), numberOfBrands)
-        linksCollector = LinksCollector(self.separateDBname)
-        numberOfLinks = linksCollector.Collect(limit=100)
+        linksCollector = LinksCollector(self.database)
+        numberOfLinks, linksStartTime = linksCollector.Collect(limit=100)
         self.assertFalse(linksCollector.db.valueIsPresentInColumnOfATable("True", "parsed", "links"))
         self.assertGreater(numberOfLinks, 100)
         self.assertEqual(linksCollector.db.countRecordsInTable("links"), numberOfLinks)
+        self.assertLess((datetime.datetime.now() - linksStartTime).total_seconds(), 240)
 
-        carsCollector = CarsCollector(self.separateDBname)
-        numberOfCars = carsCollector.Collect()
+        carsCollector = CarsCollector(self.database)
+        numberOfCars, carsStartTime = carsCollector.Collect()
         self.assertGreater(numberOfCars, 0)
         self.assertLess(numberOfCars, numberOfLinks)
         self.assertEqual(carsCollector.db.countRecordsInTable("cars_car"), numberOfCars)
+        self.assertLess((datetime.datetime.now() - carsStartTime).total_seconds(), 480)
 
 class CombinedCollectorsTest(unittest.TestCase):
     def setUp(self):
         self.combinedDBname = "combined_collectors_test.db"
 
+        if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), self.combinedDBname)):
+            os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), self.combinedDBname))
+
     def tearDown(self):
+        pass
         os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), self.combinedDBname))
 
     def testCombinedCollectors(self):
